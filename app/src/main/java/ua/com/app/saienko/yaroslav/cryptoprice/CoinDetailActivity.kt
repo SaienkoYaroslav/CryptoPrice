f570fc6d5e46624ed647fa2a8b628ed92be0eab0
SaienkoYaroslav <saienko.yaroslav01@gmail.com>
2023-03-03 22:53:09 +0200

    view binding everywhere

--- app/src/main/java/ua/com/app/saienko/yaroslav/cryptoprice/CoinDetailActivity.kt
+++ app/src/main/java/ua/com/app/saienko/yaroslav/cryptoprice/CoinDetailActivity.kt
@@ -3,32 +3,25 @@ package ua.com.app.saienko.yaroslav.cryptoprice
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.squareup.picasso.Picasso
+import ua.com.app.saienko.yaroslav.cryptoprice.databinding.ActivityCoinDetailBinding
 import java.text.DecimalFormat
 
 class CoinDetailActivity : AppCompatActivity() {
 
     private lateinit var viewModel: CoinViewModel
-    private lateinit var ivLogoCoin: ImageView
-    private lateinit var tvFromSymbol: TextView
-    private lateinit var tvToSymbol: TextView
-    private lateinit var tvPrice: TextView
-    private lateinit var tvMinPrice: TextView
-    private lateinit var tvMaxPrice: TextView
-    private lateinit var tvLastDeal: TextView
-    private lateinit var tvUpdated: TextView
+
+    private lateinit var binding: ActivityCoinDetailBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_coin_detail)
-
-        init()
+        binding = ActivityCoinDetailBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         viewModel = ViewModelProvider(
             this,
@@ -45,17 +38,17 @@ class CoinDetailActivity : AppCompatActivity() {
         val formattedDouble = DecimalFormat("#0.000000000")
 
         viewModel.getDetailInfo(fromSymbol!!).observe(this, Observer {
-            tvPrice.text =
+            binding.tvPrice.text =
                 if (it.price?.compareTo(0.1) == -1) formattedDouble.format(it.price) else it.price.toString()
-            tvMinPrice.text =
+            binding.tvMinPrice.text =
                 if (it.lowDay?.compareTo(0.1) == -1) formattedDouble.format(it.lowDay) else it.lowDay.toString()
-            tvMaxPrice.text =
+            binding.tvMaxPrice.text =
                 if (it.highDay?.compareTo(0.1) == -1) formattedDouble.format(it.highDay) else it.highDay.toString()
-            tvLastDeal.text = it.lastMarket
-            tvUpdated.text = it.getFormattedTime()
-            tvFromSymbol.text = it.fromSymbol
-            tvToSymbol.text = it.toSymbol
-            Picasso.get().load(it.getFullImageUrl()).into(ivLogoCoin)
+            binding.tvLastDeal.text = it.lastMarket
+            binding.tvUpdated.text = it.getFormattedTime()
+            binding.tvFromSymbol.text = it.fromSymbol
+            binding.tvToSymbol.text = it.toSymbol
+            Picasso.get().load(it.getFullImageUrl()).into(binding.ivLogoCoin)
         })
     }
 
@@ -68,17 +61,6 @@ class CoinDetailActivity : AppCompatActivity() {
             intent.putExtra(EXTRA_FROM_SYMBOL, fromSymbol)
             return intent
         }
-    }
-
-    private fun init() {
-        ivLogoCoin = findViewById(R.id.ivLogoCoin)
-        tvFromSymbol = findViewById(R.id.tvFromSymbol)
-        tvToSymbol = findViewById(R.id.tvToSymbol)
-        tvPrice = findViewById(R.id.tvPrice)
-        tvMinPrice = findViewById(R.id.tvMinPrice)
-        tvMaxPrice = findViewById(R.id.tvMaxPrice)
-        tvLastDeal = findViewById(R.id.tvLastDeal)
-        tvUpdated = findViewById(R.id.tvUpdated)
     }
 
 }
